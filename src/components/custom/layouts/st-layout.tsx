6445f88740906e8299b3061507c34005d1b61d66
omer-os <omerchetin19@gmail.com>
2024-04-28 16:27:01 +0300

    s

--- src/components/custom/layouts/st-layout.tsx
+++ src/components/custom/layouts/st-layout.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import StNavbar from "../navbars/st-navbar";
 import StFooter from "../footers/st-footer";
+import { getStoreInformation } from "~/server/actions/store";
 
-export default function StLayout({ children }: { children: React.ReactNode }) {
+export default async function StLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeInfo = await getStoreInformation();
   return (
     <>
       <div className="min-h-screen">
-        <StNavbar />
+        <StNavbar store={storeInfo[0]} />
         {children}
       </div>
       <StFooter />
--- src/components/custom/navbars/st-navbar.tsx
+++ src/components/custom/navbars/st-navbar.tsx
@@ -7,15 +7,16 @@ import MyUserAvatar from "../avatars/my-user-avatar";
 import { LogIn, ShoppingBag } from "lucide-react";
 import StCartSheet from "../sheets/st-cart-sheet";
 import Link from "next/link";
+import { Store } from "@prisma/client";
 
-export default async function StNavbar() {
+export default async function StNavbar({ store }: { store?: Store }) {
   const session = await getServerAuthSession();
 
   return (
     <nav className="sticky left-0 top-0 z-30 h-[60px] border-b bg-secondary/40 backdrop-blur-xl">
       <div className="container mx-auto flex h-full items-center justify-between">
         <Link href="/">
-          <div className="text-xl font-semibold">Store Name</div>
+          <div className="text-xl font-semibold">{store?.name || "Store"}</div>
         </Link>
 
         <div className="flex items-center gap-4">
